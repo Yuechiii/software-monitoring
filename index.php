6957913d81e3df9bff3d81b019e4bb30c0c9a2dc
Kent Ryner Bawiga <kentryner21@gmai.com>
2025-12-10 20:18:29 +0800

    added tailwind and a few components

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+
+    header("Location: pages/validation.php");  // Redirect to login page
+    exit();
+     
+
---
+++ views/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/css/global.css">
+    <title>Document</title>
+</head>
+
+<body class="relative min-h-screen">
+    
+</body>
+
+</html>
